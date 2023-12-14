38a685f73cefcd58c0a19e061fa6c604bf4f3966
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-14 17:44:47 +0900

    :heavy_plus_sign: :: data module build.gradle.kts 작성
    :: data module build.gradle.kts 작성

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -1,43 +1,56 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id(Dependency.Gradle.LIBRARY)
+    id(Dependency.Gradle.KOTLIN)
+    kotlin(Dependency.Gradle.KAPT)
 }
 
+@Suppress("UnstableApiUsage")
 android {
-    namespace = "com.idea_festival.data"
-    compileSdk = 33
+    namespace = ProjectProperties.NameSpace.DATA
+    compileSdk = ProjectProperties.Versions.COMPILE_SDK
 
     defaultConfig {
-        minSdk = 24
+        minSdk = ProjectProperties.Versions.MIN_SDK
+        targetSdk = ProjectProperties.Versions.TARGET_SDK
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+        testInstrumentationRunner = ProjectProperties.Test.TEST_RUNNER
+        consumerProguardFiles(ProjectProperties.Files.CONSUMER_PROGUARDFILES)
+        buildTypes {
+            release {
+                isMinifyEnabled = false
+                proguardFiles(
+                    getDefaultProguardFile(ProjectProperties.Files.DEFAULT_PROGUARDFILES),
+                    ProjectProperties.Files.PROGUARDFILES
+                )
+            }
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
+        compileOptions {
+            sourceCompatibility = ProjectProperties.Versions.JAVA_VERSION
+            targetCompatibility = ProjectProperties.Versions.JAVA_VERSION
+        }
+        kotlinOptions {
+            jvmTarget = ProjectProperties.Versions.JVM_TARGET
+        }
     }
 }
 
 dependencies {
+    implementation(project(":domain"))
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(Dependency.AndroidX.CORE_KTX)
+    implementation(Dependency.AndroidX.APPCOMPAT)
+    implementation(Dependency.Google.MATERIAL)
+    testImplementation(Dependency.Test.JUNIT)
+    androidTestImplementation(Dependency.Test.ANDROID_JUNIT)
+    androidTestImplementation(Dependency.Test.ESPRESSO)
+
+    implementation(Dependency.Google.HILT)
+    kapt(Dependency.Google.HILT_COMPILER)
+
+    implementation(Dependency.Libraries.RETROFIT)
+    implementation(Dependency.Libraries.OKHTTP)
+    implementation(Dependency.Libraries.OKHTTP_LOGGING_INTERCEPTOR)
+    implementation(Dependency.Libraries.RETROFIT_CONVERTER_GSON)
+
+    implementation(Dependency.DataStore.PREFERENCES)
 }
