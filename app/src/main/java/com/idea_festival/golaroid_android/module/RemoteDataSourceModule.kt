327effb0874a904e5bacf6f20b31004d5e63319c
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-15 09:51:03 +0900

    :sparkles: :: RemoteDataSourceModule 작성
    :: RemoteDataSourceModule 작성

---
+++ app/src/main/java/com/idea_festival/golaroid_android/module/RemoteDataSourceModule.kt
@@ -0,0 +1,11 @@
+package com.idea_festival.golaroid_android.module
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RemoteDataSourceModule {
+
+}
