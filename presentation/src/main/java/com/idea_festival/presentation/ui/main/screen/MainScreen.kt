18ad981d817fc08d3ae7e94792a3fc9bb6fe39d6
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-16 18:51:37 +0900

    :lipstick: :: GOlaroidLogo Component 생성
    :: GOlaroidLogo Component 생성

---
+++ presentation/src/main/java/com/idea_festival/presentation/ui/main/screen/MainScreen.kt
@@ -0,0 +1,15 @@
+package com.idea_festival.presentation.ui.main.screen
+
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.runtime.Composable
+import com.idea_festival.design_system.theme.GolaroidAndroidTheme
+
+@Composable
+@OptIn(ExperimentalMaterialApi::class)
+fun MainScreen(
+    
+) {
+    GolaroidAndroidTheme { colors, typography ->  
+
+    }
+}
