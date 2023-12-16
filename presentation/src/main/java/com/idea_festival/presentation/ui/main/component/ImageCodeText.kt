2d34458514f44fee422ef6c83abb59fab7cff120
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-17 00:11:04 +0900

    :lipstick: :: PictureCardComponent 작성
    :: PictureCardComponent 작성

--- presentation/src/main/java/com/idea_festival/presentation/ui/main/component/ImageCodeText.kt
+++ presentation/src/main/java/com/idea_festival/presentation/ui/main/component/ImageCodeText.kt
@@ -4,36 +4,40 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.idea_festival.design_system.theme.GolaroidAndroidTheme
 
 @Composable
-fun ImageCodeText() {
+fun ImageCodeText(modifier: Modifier = Modifier) {
     GolaroidAndroidTheme { colors, typography ->
         Box(
-            modifier = Modifier
-                .background(color = colors.GRAY2.copy(alpha = 0.6f), shape = RoundedCornerShape(5.dp))
+            modifier = modifier
+                .background(
+                    color = colors.GRAY2.copy(alpha = 0.6f),
+                    shape = RoundedCornerShape(5.dp)
+                )
                 .padding(horizontal = 4.dp, vertical = 5.dp),
         ) {
-            Row{
-                Text(text = "이동욱 ",
+            Row(
+                modifier = Modifier.align(Alignment.Center)
+            ) {
+                Text(
+                    text = "이동욱 ",
                     style = typography.labelSmall,
                     color = colors.WHITE,
                 )
 
-                Text(text = "F239AS",
+                Text(
+                    text = "F239AS",
                     style = typography.labelSmall,
                     color = colors.WHITE
                 )
-
             }
         }
     }
--- presentation/src/main/java/com/idea_festival/presentation/ui/main/component/PictureCard.kt
+++ presentation/src/main/java/com/idea_festival/presentation/ui/main/component/PictureCard.kt
@@ -3,7 +3,13 @@ package com.idea_festival.presentation.ui.main.component
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -24,17 +30,27 @@ fun PictureCard() {
     GolaroidAndroidTheme { colors, typography ->
         Box(
             modifier = Modifier
-                .wrapContentSize()
+                .width(140.dp)
+                .height(180.dp)
         ) {
             Image(
                 modifier = Modifier
-                    .width(140.dp)
-                    .height(180.dp)
                     .clip(RoundedCornerShape(10.dp)),
                 painter = painterResource(id = com.idea_festival.golaroid_android.design_system.R.drawable.test_image),
                 contentDescription = "test image",
                 contentScale = ContentScale.Crop
             )
+
+            Row(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                ImageCodeText(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth()
+                        .padding(top = 148.dp, start = 42.dp, bottom = 6.dp, end = 6.dp)
+                )
+            }
         }
     }
 }
