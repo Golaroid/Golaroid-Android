8cf95123ce79c77fffe84c7db24e54d576629d32
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-19 00:45:27 +0900

    :lipstick: :: WantedFrame Component화
    :: WantedFrame Component화

--- design-system/src/main/java/com/idea_festival/design_system/component/frame/Frame.kt
+++ design-system/src/main/java/com/idea_festival/design_system/component/frame/Frame.kt
@@ -2,6 +2,7 @@ package com.idea_festival.design_system.component.frame
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -240,12 +242,85 @@ fun WinterFrame() {
         }
     }
 }
+
+@Composable
+fun WantedFrame() {
+    Box(
+        modifier = Modifier
+            .width(180.dp)
+            .height(600.dp)
+            .background(color = Color(0xFFEBD4BE))
+            .border(width = 4.dp, color = Color(0xFF55463C))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.wanted_frame_decoration),
+            contentDescription = null,
+            modifier = Modifier.padding(top = 14.dp, bottom = 10.dp)
+                .align(Alignment.TopCenter)
+        )
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .align(Alignment.Center)
+                .zIndex(-1f)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.test_image_one_piece),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(top = 37.dp, bottom = 7.dp)
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
+                    .height(120.dp)
+                    .clip(shape = RoundedCornerShape(5.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.test_image_one_piece),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
+                    .height(115.dp)
+                    .clip(shape = RoundedCornerShape(5.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.test_image_one_piece),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(bottom = 8.dp)
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
+                    .height(115.dp)
+                    .clip(shape = RoundedCornerShape(5.dp)),
+                contentScale = ContentScale.Crop
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.test_image_one_piece),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 12.dp)
+                    .height(115.dp)
+                    .clip(shape = RoundedCornerShape(5.dp)),
+                contentScale = ContentScale.Crop
+            )
+        }
+    }
+}
+
+
 @Preview
 @Composable
 fun FramePre() {
     Row(
         modifier = Modifier.fillMaxSize()
     ) {
-        WinterFrame()
+        WantedFrame()
     }
 }
