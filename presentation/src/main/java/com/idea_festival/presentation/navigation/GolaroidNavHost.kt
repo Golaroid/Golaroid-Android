d9931e906bbae356f259dc97f67c22cb722dd718
Chaejongin12 <s22055@gsm.hs.kr>
2023-12-20 03:06:27 +0900

    :sparkles: :: 세팅된 Navigation Navhost에 등록
    :: 세팅된 Navigation Navhost에 등록

--- presentation/src/main/java/com/idea_festival/presentation/navigation/GolaroidNavHost.kt
+++ presentation/src/main/java/com/idea_festival/presentation/navigation/GolaroidNavHost.kt
@@ -6,7 +6,10 @@ import androidx.navigation.compose.NavHost
 import com.idea_festival.presentation.ui.GolaroidAppState
 import com.idea_festival.presentation.ui.capture.navigation.captureScreen
 import com.idea_festival.presentation.ui.capture.navigation.navigateToCapture
+import com.idea_festival.presentation.ui.input_information.navigation.inputInformationScreen
 import com.idea_festival.presentation.ui.input_information.navigation.navigateToInputInformation
+import com.idea_festival.presentation.ui.input_information.navigation.navigateToUploadImageSuccess
+import com.idea_festival.presentation.ui.input_information.navigation.uploadImageSuccessScreen
 import com.idea_festival.presentation.ui.issued_code.navigation.issuedCodeScreen
 import com.idea_festival.presentation.ui.issued_code.navigation.navigateToRevealPicture
 import com.idea_festival.presentation.ui.issued_code.navigation.revealPictureScreen
@@ -16,6 +19,11 @@ import com.idea_festival.presentation.ui.main.navigation.navigateToMain
 import com.idea_festival.presentation.ui.search_result.navigation.existCodeScreen
 import com.idea_festival.presentation.ui.search_result.navigation.navigateToExistCode
 import com.idea_festival.presentation.ui.search_result.navigation.noExistCodeScreen
+import com.idea_festival.presentation.ui.select_frame.navigation.navigateToPrintSuccess
+import com.idea_festival.presentation.ui.select_frame.navigation.navigateToSelectFrame
+import com.idea_festival.presentation.ui.select_frame.navigation.selectFrameScreen
+import com.idea_festival.presentation.ui.select_image.navigation.navigateToSelectImage
+import com.idea_festival.presentation.ui.select_image.navigation.selectImageScreen
 
 @Composable
 fun GolaroidNavHost(
@@ -57,6 +65,23 @@ fun GolaroidNavHost(
         revealPictureScreen(
             onApproveButtonClick = navController::navigateToInputInformation,
             onRejectButtonClick = navController::navigateToInputInformation,
+        )
+
+        inputInformationScreen(
+            onNextButtonClick = navController::navigateToUploadImageSuccess
+        )
+
+        uploadImageSuccessScreen(
+            onCheckButtonClick = navController::navigateToSelectImage
+        )
+
+        selectImageScreen(
+            onNextButtonClick = navController::navigateToSelectFrame
+        )
+
+        selectFrameScreen(
+            onNextButtonClick = navController::navigateToMain,
+            onPrintButtonClick = navController::navigateToPrintSuccess
         )
     }
 }
