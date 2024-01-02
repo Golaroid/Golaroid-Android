a0e89060fec24ee8622d60c59eb38963f5116be4
정찬우 <105582057+wjdcksdn@users.noreply.github.com>
2024-01-02 20:21:10 +0900

    :memo: edit image -> camera

--- presentation/src/main/java/com/idea_festival/presentation/ui/GolaroidApp.kt
+++ presentation/src/main/java/com/idea_festival/presentation/ui/GolaroidApp.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import com.idea_festival.design_system.theme.GolaroidAndroidTheme
 import com.idea_festival.presentation.navigation.GolaroidNavHost
+import com.idea_festival.presentation.ui.viewmodel.CameraViewModel
 import com.idea_festival.presentation.ui.viewmodel.ImageViewModel
 import com.idea_festival.presentation.ui.viewmodel.PostViewModel
 
@@ -12,13 +13,13 @@ fun GolaroidApp(
     windowSizeClass: WindowSizeClass,
     appState: GolaroidAppState = rememberGolaroidAppState(windowSizeClass = windowSizeClass),
     postViewModel: PostViewModel,
-    imageViewModel: ImageViewModel
+    cameraViewModel: CameraViewModel
 ) {
     GolaroidAndroidTheme { _, _ ->
         GolaroidNavHost(
             appState = appState,
             postViewModel = postViewModel,
-            imageViewModel = imageViewModel
+            cameraViewModel = cameraViewModel
         )
     }
 }
